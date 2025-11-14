9650f1927c4204fe045776f4a20186f32d878090
Gaurav Jangid <jangidgaurav603@gmail.com>
2025-11-15 02:49:19 +0530

    subscribe complete

--- src/Components/Subscribe/Subscribe.tsx
+++ src/Components/Subscribe/Subscribe.tsx
@@ -1,8 +1,29 @@
 import './Subscribe.css'
+import STAR from "../../assets/images/stardown.png"
+import telegramimg from "../../assets/svg/telegram.svg"
+import mail from "../../assets/svg/mail.svg"
+
 const Subscribe = () => {
   return (
     <div className='subscribe'>
         <div className="subscribe_container">
+            <div className='empty'></div>
+            <div className='Subsribe_mail'>
+                  <div className="telegram"><img src={telegramimg} alt="" /></div>
+                  <div className="subscribe_para">
+                    <span className='real_para'> Subscribe to get information, latest news and other </span>
+                    <span className='real_para'>interesting offers about Jadoo</span>
+                  </div>
+                  <div className="mail_box">
+                    <img className='mail_img' src={mail} alt="" />
+                    <input className='mail_input' type="email" placeholder='Your email' />
+                    <button className='primary_btn'>Subscribe</button>
+                  </div>
+            </div>
+            <div className='star_content'>
+              <img src={STAR} alt="image not foound" />
+              
+            </div>
             
         </div>
     </div>
